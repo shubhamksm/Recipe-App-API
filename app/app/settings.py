083f29e8a82b6687c4dc7f5db5bf02aea0f55da1
shubhamksm <khandelwalshubham1992@gmail.com>
2020-03-28 13:03:28 +0000

    Added working list tags endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'core',
     'user',
+    'recipe',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
